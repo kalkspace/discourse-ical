7f7514bb7cf70ac9ae833d990f943270f8da5b44
Ansgar Prause <ansgarprause@gmail.com>
2023-04-16 16:36:14 +0200

    Restore content-type

--- src/handler.ts
+++ src/handler.ts
@@ -90,7 +90,9 @@ const handle: Handler = async (request) => {
   for (const calendarEvent of calendarEvents) {
     calendar.createEvent(calendarEvent);
   }
-  return new Response(calendar.toString());
+  return new Response(calendar.toString(), {
+    headers: { "Content-Type": "text/calendar" },
+  });
 };
 
 serve(handle);
